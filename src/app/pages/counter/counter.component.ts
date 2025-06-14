50b20b73781693f30d84315c541447ee7934a895
marco-soria <marco.soriaa7@gmail.com>
2025-06-14 17:32:14 -0500

    introsignal

--- src/app/pages/counter/counter.component.ts
+++ src/app/pages/counter/counter.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-counter',
@@ -8,16 +8,20 @@ import { Component } from '@angular/core';
 })
 export class CounterComponent {
   counter = 10;
+  counterSignal = signal(10);
 
   increaseBy(value: number): void {
     this.counter += value;
+    this.counterSignal.update((current) => current + value);
   }
 
   decreaseBy(value: number): void {
     this.counter -= value;
+    this.counterSignal.update((current) => current - value);
   }
 
   reset(): void {
     this.counter = 10;
+    this.counterSignal.set(10);
   }
 }
